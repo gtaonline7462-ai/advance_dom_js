ee40a69a657e46b38dc829510c70a3c2c712f941
pourya vahid <pourya.v185@gmail.com>
2025-12-15 18:08:25 +0330

    varsion--3

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header')
+const btnScroll = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+const tap = document.querySelectorAll('.operations__tab')
+const tapcontainer = document.querySelector('.operations__tab-container')
+
+const tapsContent = document.querySelectorAll('.operations__content')
+
 const openModal = function (e) {
   e.preventDefault()
   console.log(e.target);
@@ -31,6 +38,18 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//smooth scroll
+btnScroll.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: "smooth" })
+})
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault()
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
+})
+
 ////////////////////////////////////////////
 
 // const massage = document.createElement('div')
@@ -42,4 +61,68 @@ document.addEventListener('keydown', function (e) {
 //   massage.remove()
 // })
 
-document.documentElement.style.setProperty('--color-primary', '#ff585f')
+// برای دست پیدا کردن به قسمت root css
+
+// document.documentElement.style.setProperty('--color-primary', '#ff585f')
+
+// btnScroll.addEventListener('click', function (e) {
+//   const rect = section1.getBoundingClientRect()
+//   console.log(rect);
+//   // console.log(e.target.getBoundingClientRect());
+//   console.log(window.pageXOffset, pageYOffset);
+//   window.scrollTo({
+//     top: rect.top + window.pageYOffset,
+//     left: rect.left + pageXOffset,
+//     behavior: "smooth"
+//   })
+//  section1.scrollIntoView({ behavior: 'smooth' })
+// })
+// const h1 = document.querySelector('h1')
+// const alertevent = function (e) {
+//   alert('hi')
+// }
+// h1.addEventListener('mouseenter', alertevent
+// )
+// setTimeout(() => h1.removeEventListener('mouseenter', alertevent), 10000)
+
+
+//////////////////////
+//create random color
+// const randomint = (min, max) => Math.floor(Math.random() * (max - min) + min)
+// const randomColor = () => {
+//   return `rgb(${randomint(0, 255)},${randomint(0, 255)},${randomint(0, 255)})`
+// };
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+
+
+//   this.style.backgroundColor = randomColor()
+// })
+////////////////////////////////////////
+// document.querySelectorAll('.nav__link').forEach((el) =>
+
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     const id = el.getAttribute('href')
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+//   })
+// )
+
+
+
+tapcontainer.addEventListener('click', function (e) {
+  e.preventDefault()
+
+
+  const clicked = e.target.closest('.operations__tab')
+  if (!clicked) return;
+  tap.forEach(t => t.classList.remove('operations__tab--active'))
+  if (clicked) { clicked.classList.add('operations__tab--active') }
+
+  tapsContent.forEach(tp => tp.classList.remove('operations__content--active'))
+
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+
+
+})
