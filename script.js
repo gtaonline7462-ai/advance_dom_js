2e7983aaa2631b01d628cfe2890678321e37ef73
pourya vahid <pourya.v185@gmail.com>
2025-12-18 19:59:50 +0330

    days five

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
 const btnScroll = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
+const section = document.querySelectorAll('.section')
 const tap = document.querySelectorAll('.operations__tab');
 const tapcontainer = document.querySelector('.operations__tab-container');
 
@@ -146,3 +149,159 @@ nav.addEventListener('mouseout', function (e) {
   e.preventDefault();
   hoverhandeler(e, 1);
 });
+
+
+
+// sticky nav
+let visible = null
+const obscallback = (entries) => {
+  visible = entries[0].isIntersecting;
+  if (!visible) {
+    nav.classList.add('sticky')
+  } else {
+    nav.classList.remove('sticky')
+  }
+
+}
+
+const observer = new IntersectionObserver(obscallback, {
+  threshold: 0, root: null,
+  rootMargin: '-90px'
+})
+observer.observe(document.querySelector('.header'))
+
+
+//reveal hidden
+const sectionhidden = (entries, observer) => {
+  entries.forEach((el) => {
+    if (!el.isIntersecting) {
+      return
+    }
+    el.target.classList.remove('section--hidden')
+    observer.unobserve(el.target)
+  })
+
+}
+const hiddenobserve = new IntersectionObserver(sectionhidden, { root: null, threshold: 0.3 })
+hiddenobserve.observe(section1)
+hiddenobserve.observe(section2)
+hiddenobserve.observe(section3)
+
+//lazy loading image
+
+const imgtarget = document.querySelectorAll('img[data-src]')
+const loadimg = function (entries, observe) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) return
+    entry.target.src = entry.target.dataset.src
+    entry.target.addEventListener('load', function (e) {
+      entry.target.classList.remove('lazy-img')
+    })
+    observe.unobserve(entry.target)
+  })
+}
+const imgobserver = new IntersectionObserver(loadimg, {})
+imgtarget.forEach((img) => {
+  imgobserver.observe(img)
+})
+
+//create slider
+// const slider = document.querySelector('.slider');
+// const slides = document.querySelectorAll('.slide');
+
+// // اول فقط اسلاید اول رو نگه می‌داریم
+// for (let i = 1; i < slides.length; i++) {
+//   slider.removeChild(slides[i]);
+// }
+// let currentIndex = 0;
+// slider.addEventListener('click', function (e) {
+//   if (e.target.classList.contains('slider__btn--right')) {
+//     if (currentIndex <= slides.length - 1) {
+//       slider.removeChild(slides[currentIndex]);
+//       currentIndex = (currentIndex + 1) % slides.length
+//       slider.appendChild(slides[currentIndex]);
+//     }
+//   }
+// });
+
+const slides = document.querySelectorAll('.slide');
+const btnright = document.querySelector('.slider__btn--right')
+const btnleft = document.querySelector('.slider__btn--left')
+let curslide = 0
+let maxslid = slides.length
+
+// firstslide
+slides.forEach(function (s, i) {
+  if (i <= slides.length)
+    s.style.transform = `translateX(${100 * (i)}%)`
+})
+
+const gotoSlide = function (curslide) {
+  slides.forEach(function (s, i) {
+    if (i <= slides.length)
+      s.style.transform = `translateX(${100 * (i - curslide)}%)`
+  })
+}
+
+//next slide
+
+//after
+btnright.addEventListener('click', function (e) {
+  if (curslide == maxslid - 1) {
+    curslide = 0
+  } else { curslide++ }
+  gotoSlide(curslide)
+  activedot(curslide)
+}
+)
+//previuse
+btnleft.addEventListener('click', function (e) {
+  if (curslide == 0) {
+    curslide = maxslid - 1
+  } else { curslide-- }
+  gotoSlide(curslide)
+  activedot(curslide)
+})
+//keyboard
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') {
+    if (curslide == maxslid - 1) {
+      curslide = 0
+    } else { curslide++ }
+    gotoSlide(curslide)
+    activedot(curslide)
+  }
+  if (e.key === 'ArrowLeft') {
+    if (curslide == 0) {
+      curslide = maxslid - 1
+    } else { curslide-- }
+    gotoSlide(curslide)
+    activedot(curslide)
+  }
+})
+// create dot
+const dotcontainer = document.querySelector('.dots')
+slides.forEach(function (s, i) {
+  dotcontainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+})
+//activate dot
+const activedot = function (slide) {
+  // همه‌ی دکمه‌ها رو غیر فعال کن
+  document.querySelectorAll('.dots__dot').forEach(function (d) {
+    d.classList.remove('dots__dot--active');
+  });
+
+  // دکمه‌ی مربوط به اسلاید فعلی رو فعال کن
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+
+activedot(0)
+dotcontainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    curslide = Number(e.target.dataset.slide)
+    gotoSlide(curslide)
+    activedot(curslide)
+  }
+})
